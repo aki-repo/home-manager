2fac455a408f72102ffb670abf1f5d5841894be2
Andreas Taenzer <andreast.work@gmail.com>
2025-12-28 09:19:02 -0500

    fix

--- gui/walker.nix
+++ gui/walker.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }
-
+{
+ nix.settings = {
+  extra-substituters = ["https://walker.cachix.org" "https://walker-git.cachix.org"];
+  extra-trusted-public-keys = ["walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM=" "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="];
+};
 programs.walker = {
   enable = true;
   runAsService = true; # Note: this option isn't supported in the NixOS module only in the home-manager module
